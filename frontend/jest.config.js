812393a644c6f0a713952be6bb93702695313e36
Huy <huynguyenvt1989@gmail.com>
2025-06-12 22:05:09 -0400

    added date field for Log Task modal

---
+++ frontend/jest.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['<rootDir>/tests/setup.js'],
+  testMatch: [
+    '<rootDir>/tests/**/*.test.js'
+  ],
+  collectCoverageFrom: [
+    '**/*.js',
+    '!**/node_modules/**',
+    '!**/tests/**'
+  ]
+};
